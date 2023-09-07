10867e7c3233063c7b5f3cae3212abca14050685
sampath88 <sampathreddy8896@gmail.com>
2023-09-07 21:47:56 +0530

    added api-docs
    added routes and controller
    
    added logger

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express = require("express");
-
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const app = express();
+const fileUpload = require("express-fileupload");
+
+//swagger documentation
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//cookies and file middleware
+app.use(cookieParser());
+app.use(fileUpload());
+
+//morgan middleware - logger
+app.use(morgan("tiny"));
+
+//import all routes here
+const home = require("./routes/home.route");
+
+//router middleware
+app.use("/api/v1", home);
 
 //export app
 module.exports = app;
