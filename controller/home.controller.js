b04afae3772da8993c446690b6dbdf6ac9078b8f
sampath88 <sampathreddy8896@gmail.com>
2023-09-07 22:02:49 +0530

    added BigPromise middleware

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -1,9 +1,11 @@
-exports.home = (req, res) => {
+const BigPromise = require("../middleware/BigPromise");
+
+exports.home = BigPromise((req, res) => {
   res.status(200).json({
     success: true,
     greeting: "Hello from API",
   });
-};
+});
 
 exports.homeDummy = (req, res) => {
   res.status(200).json({
